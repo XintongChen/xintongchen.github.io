4f79306188abb485fc7899eab9e9f6f5d9fdb3a2
xintong <xintong0308@gmail.com>
2015-12-16 12:07:46 -0500

    new portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+function goTo(el){
+      var ID = "#" + el;
+      $('body, html').animate({scrollTop:$(ID).offset().top}, '700');
+  	}
+
+$('.intro .fullName').transition({ y: '15px',opacity: 1 },1000);
+$('.intro .keywords').transition({ y: '-10px',opacity: 1,delay:100 },900);
+$('.intro .selfIntro').transition({opacity: 1},700);
+
+$(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+});
+
+
