3fd2b65efc447edb86d48e5a1ce9920c2ca01bce
Claire Chen <xc2282@tc.columbia.edu>
2018-05-04 17:04:56 -0400

    update

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
+function goTo(el) {
+    var ID = "#" + el;
+    $(".mobile-nav").fadeOut('400');
+    $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
+}
+
+
+$('[data-toggle="tooltip"]').tooltip()
+
+
+
 $(window).load(function() {
-   $(".loading").fadeOut(500);
+    $(".loading").fadeOut(500);
 });
 
+// $('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
 
+// $('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
+$(".menu-trigger").click(function(event) {
+    $(".mobile-nav").slideDown('400');
+});
 
+$(".menu-closer").click(function(event) {
+    $(".mobile-nav").fadeOut('400');
+        
+   
+});
 
- function goTo(el) {
-     var ID = "#" + el;
-     $('body, html').animate({ scrollTop: $(ID).offset().top }, '700');
- }
+$(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+});
 
- // $('.intro .fullName').transition({ y: '15px', opacity: 1 }, 1000);
- $('.intro .keywords').transition({ y: '10px', opacity: 1, delay: 100 }, 900);
- $('.intro .introduction').transition({ y: '-10px', opacity: 1, delay: 100 }, 700);
+$(".homeKey").click(function() {
+    window.location.href = "index.html";
+});
 
- $(".navbar-nav li a").click(function(event) {
-     $(".navbar-collapse").collapse('hide');
- });
+$(".topKey").click(function() {
+    $('body, html').animate({ scrollTop: 0 }, '700');
+});
 
- $(".homeKey").click(function() {
-     window.location.href = "index.html";
- });
-
- $(".topKey").click(function() {
- 	$('body, html').animate({ scrollTop: 0 }, '700');
- });
-
- $(window).scroll(function() {
- 	 if ($(window).scrollTop() > 800) {
- 	 	$(".topKey").fadeIn();
- 	 } else {
- 	 	$(".topKey").fadeOut();
- 	 }
- });
-
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 800) {
+        $(".topKey").fadeIn();
+    } else {
+        $(".topKey").fadeOut();
+    }
+});
 
 $('.carousel').carousel();
 
-
-
-
-            // $(".icon-drawing").find("path").addClass("line-drawing");
-       
- 
+$("[data-fancybox]").fancybox();
